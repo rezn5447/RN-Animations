eb6dfb1fd2a6b27f6897ead604c5f1a20a6c8090
Resdan Logan <rezn5447@gmail.com>
2018-02-28 12:12:24 -0800

    panResponder to Deck

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ export default class App extends Component {
       </Card>
     );
   }
+
   render() {
     return (
       <View style={styles.container}>
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, PanResponder, Animated } from 'react-native';
 
 export default class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        console.log(gesture);
+      },
+      onPanResponderRelease: () => {}
+    });
+
+    this.panResponder = panResponder;
+  }
+
   renderCards() {
     return this.props.data.map(item => {
       return this.props.renderCard(item);
     });
   }
   render() {
-    return <View>{this.renderCards()}</View>;
+    return <View {...this.panResponder.panHandlers}>{this.renderCards()}</View>;
   }
 }
 
